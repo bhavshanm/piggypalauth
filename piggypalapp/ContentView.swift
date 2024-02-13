8e7f5591ce6549830638267e8188c648128e3851
Rithmika Naveen <rithmin7@gmail.com>
2024-02-12 21:26:53 -0800

    Initial Commit

---
+++ piggypalapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  piggypalapp
+//
+//  Created by Rithmika Naveen on 2/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ piggypalapp/piggypalappApp.swift
@@ -0,0 +1,17 @@
+//
+//  piggypalappApp.swift
+//  piggypalapp
+//
+//  Created by Rithmika Naveen on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct piggypalappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
